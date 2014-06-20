cce14cf80378ef7937d16d4062d0e60cc03ae607
bstuart12 <bstuart12@hotmail.com>
2014-06-20 11:29:23 +0200

    Fixed voor de ontbrekende vlaggen
    en een plaatje dat niet gebruikt word weg

--- src/com/epic/score_app/viewlayer/adapters/TeamItemAdapter.java
+++ src/com/epic/score_app/viewlayer/adapters/TeamItemAdapter.java
@@ -61,8 +61,8 @@ public class TeamItemAdapter extends ArrayAdapter<Team>{
 
         Tuple<Team, ImageView> teamTuple = new Tuple<Team, ImageView>(t, flag);
 		String link =chekifnotvalid(t);
-		String link_ ="http://flagspictures.org/photo/icons/normal/300/Brazil.png";
-        Bitmap bitmap = getBitmapFromMemCache("flag_"+t.getName().toLowerCase()+".png");
+		//String link_ ="http://flagspictures.org/photo/icons/normal/300/Brazil.png";
+        Bitmap bitmap = getBitmapFromMemCache("flag_"+t.getName()+".png");
         if (bitmap==null) {
         	 ServiceProvider.getInsance().getImageFromUrl(link,mMemoryCache, teamTuple);
         	
@@ -71,8 +71,6 @@ public class TeamItemAdapter extends ArrayAdapter<Team>{
 			flag.setImageBitmap(bitmap);
 			Log.i("getting from cash", "yes");
 		}
-       
-		//http://www.activityvillage.co.uk/world-cup-for-kids
 		return view;
 
 	}
@@ -98,14 +96,14 @@ public class TeamItemAdapter extends ArrayAdapter<Team>{
 	
 	public String chekifnotvalid(Team land)
 	{
-		String link ="http://www.activityvillage.co.uk/sites/default/files/images/"+land.getName().toLowerCase().toString()+"_flag_printables_av2.jpg";
 		String linkhost = "http://scoreapp.freeiz.com/img/"+land.getName()+".png";
-		String link_ ="http://flagspictures.org/photo/icons/normal/300/Brazil.png";
-		String s =land.getName().toLowerCase();
+		String link = "http://scoreapp.freeiz.com/img/"+land.getName()+".png";
+		//String link_ ="http://flagspictures.org/photo/icons/normal/300/Brazil.png";
+		String s = land.getName();
 		
 		if (s.equals("bosnia-herzegovina")||s.equals("costa-rica")||s.equals("cote-divoire")||s.equals("korea-republic")||s.equals("united-states")) {
 			
-			return link_;
+			return link;
 		}else{
 			return linkhost;
 			
