2e03829709b60731816654020ce65fa4c3e523b2
Jelmerpak <jelmerpak1992@hotmail.com>
2014-06-22 15:46:18 +0200

    Wedstrijden xml + activity + adapter

--- src/com/epic/score_app/viewlayer/WedstrijdenActivity.java
+++ src/com/epic/score_app/viewlayer/WedstrijdenActivity.java
@@ -1,25 +1,52 @@
 package com.epic.score_app.viewlayer;
 
-import com.epic.score_app.view.R;
-import com.epic.score_app.view.R.layout;
-import com.epic.score_app.view.R.menu;
+import java.util.ArrayList;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.epic.score_app.serviceslayer.ServiceProvider;
+import com.epic.score_app.view.R;
+import com.epic.score_app.viewlayer.adapters.MatchItemAdapter;
+import com.epic.score_app.viewlayer.adapters.PlayerItemAdapter;
+
+import domainmodel.Match;
+import domainmodel.Player;
 
 public class WedstrijdenActivity extends Activity {
+	private ArrayList<Match> match= new ArrayList<Match>();
+	private MatchItemAdapter adapter;
+	private ListView wedstrijden_lijst;
+	private int offset=0;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_wedstrijden);
-		// Show the Up button in the action bar.
+		adapter = new MatchItemAdapter(this, match);
+		wedstrijden_lijst=(ListView) findViewById(R.id.ListViewMatch);
+		wedstrijden_lijst.setAdapter(adapter);
+		wedstrijden_lijst.setDivider(new ColorDrawable(0xff444444));
+		wedstrijden_lijst.setDividerHeight(1);
+		//wedstrijden_lijst.setOnItemClickListener(onplayerClick);
 		setupActionBar();
+	}
+	
+	
+	@Override
+	protected void onStart() {
+		loadMatches();
+		super.onStart();
 	}
 
 	/**
@@ -55,5 +82,33 @@ public class WedstrijdenActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	
+	
+	public void loadMatches(){
+		Bundle b = new Bundle();
+		b.putInt("requestcode", ServiceProvider.getmatches);
+		b.putInt("limit", 20);
+		b.putInt("offset", 0);
+		ServiceProvider.getInsance().getData(b, matchhandler);
+	}
+
+	private Handler matchhandler = new Handler(){
+		@Override
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case ServiceProvider.getmatches_response:
+				
+				match= (ArrayList<Match>) msg.obj;
+				
+				adapter.addAll(match);
+				offset+=20;
+				break;
+			default:
+				break;
+			}
+		}
+	};	
+
 
 }
---
+++ src/com/epic/score_app/viewlayer/adapters/MatchItemAdapter.java
@@ -0,0 +1,69 @@
+package com.epic.score_app.viewlayer.adapters;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
+
+import com.epic.score_app.view.R;
+import com.epic.score_app.viewlayer.SpelerActivity;
+import com.epic.score_app.viewlayer.ViewPlayer;
+
+import domainmodel.Match;
+import domainmodel.News;
+import domainmodel.Player;
+
+public class MatchItemAdapter extends ArrayAdapter<Match> {
+	private List<Match> match;
+	private Activity context;
+	private LayoutInflater inflator;
+	
+
+	public MatchItemAdapter(Activity context,List<Match> match) {
+		super(context, R.layout.view_wedstrijd_item, match);
+		this.context=context;
+		this.match=match;
+		 inflator = this.context.getLayoutInflater();
+	
+		
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+	    View view =null;
+	 view=	inflator.inflate(R.layout.view_wedstrijd_item, null);
+	Match _match = match.get(position);
+	
+	TextView matchdatum  = (TextView)view.findViewById(R.id.matchdatum);
+	TextView tijd= (TextView)view.findViewById(R.id.tijd);
+	TextView thuis = (TextView)view.findViewById(R.id.thuisploeg);
+	TextView uit = (TextView)view.findViewById(R.id.uitploeg);
+	matchdatum.setText(_match.getMatchDate());
+	tijd.setText(_match.getBeginsAt());
+	thuis.setText(_match.getTeamHome().toString());
+	uit.setText(_match.getTeamVisitor().toString());
+		
+		 
+		 
+		 
+		 
+		
+		
+		
+		
+		return view;
+	}
+	
+	
+	
+	
+}
